33eba80d97f03f1c736d94e0dc5a368eff97cefc
lily.shan <shandan.flora@gmail.com>
2017-05-04 16:14:22 +0800

    commit for new config network

---
+++ src/main/java/com/ecovacs/test/activity/ConnectGuide1Activity.java
@@ -0,0 +1,10 @@
+package com.ecovacs.test.activity;
+
+/**
+ * Created by ecosqa on 17/4/26.
+ */
+public class ConnectGuide1Activity {
+    private static ConnectGuide1Activity connectGuide1Activity = null;
+
+
+}
